01d1a9dcd15475b640db7f181b992c99c80bf7c1
YOGAYATA VERMA <63913693+yogayataverma@users.noreply.github.com>
2024-10-26 23:31:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "https://sweetconnectify.netlify.app/",  // Frontend origin
+        origin: "https://sweetconnectify.netlify.app",  // Frontend origin
         methods: ["GET", "POST"],
         credentials: true
     }
@@ -17,7 +17,7 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(cors({
-    origin: "https://sweetconnectify.netlify.app/",
+    origin: "https://sweetconnectify.netlify.app",
     credentials: true
 }));
 app.use(express.json());
